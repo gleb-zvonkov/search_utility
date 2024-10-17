3faeb863683d6d998d15336729eed6efcbff17f7
Gleb Zvonkov <glebzvonkov@Glebs-MacBook-Pro.local>
2024-10-17 14:13:57 -0400

    multiple file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser; //used to parse arguments
+use glob::glob;
 use std::fs; //used to open file
 use std::io::BufRead; //use to read the file line by line
 
@@ -7,34 +8,41 @@ struct Args {
     /// The search term
     search_term: String,
     /// The file path to search in
-    file_path: String,
+    file_paths: Vec<String>,
 }
 
 fn main() {
     let args = Args::parse(); //function is provided by Parser, which clap macro derives for the Args struct
     println!(
-        "Searching for '{}' in file '{}'",
+        "Searching for '{}' in file '{:?}'",
         args.search_term,
-        args.file_path // ask for the two argument
+        args.file_paths // ask for the two argument
     );
 
-    let file = fs::File::open(&args.file_path).unwrap(); //open file    unwrap cause panic if err and returns file object if ok
-    let reader = std::io::BufReader::new(file); //open bufreader
+    for file_path in args.file_paths {
+        for entry in glob(&file_path).unwrap() {
+            // Use glob to find all matching files and go through them
+            let path = entry.unwrap();
 
-    let mut found = false; //boolean for if search term is found
+            let file = fs::File::open(&path).unwrap(); //open file    unwrap cause panic if err and returns file object if ok
+            let reader = std::io::BufReader::new(file); //open bufreader
 
-    for (index, line) in reader.lines().enumerate() {
-        //go through all the lines in, enumarte returns index of line and the line itself, BufRead is used here
-        let line = line.unwrap(); //unwrap cause panic if err and returns line if ok
-        if line.contains(&args.search_term) {
-            //check if the line contains the search term
-            println!("{}", line);
-            //println!("Found on line {}: {}", index + 1, line); //print the line
-            found = true; //have found the search term
-        }
-    }
-    if !found {
-        // if its not found
-        println!("\nSearch term '{}' not found in the file", args.search_term);
-    }
+            let mut found = false; //boolean for if search term is found
+
+            for (index, line) in reader.lines().enumerate() {
+                //go through all the lines in, enumarte returns index of line and the line itself, BufRead is used here
+                let line = line.unwrap(); //unwrap cause panic if err and returns line if ok
+                if line.contains(&args.search_term) {
+                    //check if the line contains the search term
+                    println!("{}", line);
+                    println!("Found on line {}: {}", index + 1, line); //print the line
+                    found = true; //have found the search term
+                }
+            }
+            if !found {
+                // if its not found
+                println!("\nSearch term '{}' not found in the file", args.search_term);
+            }
+        } // end for glob loop
+    } //end for filepaths loop
 }
